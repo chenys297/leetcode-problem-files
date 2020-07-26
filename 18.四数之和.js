3443c127552e0aa32852701afc0e2d2d80ea975e
chenys297 <lip.smith297@gmail.com>
2020-07-26 23:40:45 +0800

    add explanatory note and change last commit message to "add a solution to problem '18. 4Sum'"

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -13,13 +13,17 @@
 var fourSum = function (nums, target) {
   let retArr = [];
   nums.sort((a, b) => a - b);
+  // 两层循环加左右指针实现
   for (let i = 0; i < nums.length - 3; i++) {
+    // 跳过重复的值
     if (nums[i] === nums[i - 1]) continue;
     for (let j = i + 1; j < nums.length - 2; j++) {
+      // 跳过重复的值
       if (j > i + 1 && nums[j] === nums[j - 1]) continue;
       let left = j + 1;
       let right = nums.length - 1;
       while (left !== right) {
+        // 跳过重复的值
         if (left > j + 1 && nums[left] === nums[left - 1]) {
           left++;
         } else if (right < nums.length - 1 && nums[right] === nums[right + 1]) {
